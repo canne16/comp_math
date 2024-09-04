3550db2f3fdc77968d685aa0c89113b0150c4d38
Kirill Madorskii <madorskii.kv@phystech.su>
2024-09-04 22:46:15 +0300

    Added matrix generator (integers)

---
+++ matrix_generator.py
@@ -0,0 +1,42 @@
+import numpy as np
+import random
+
+def generate_inv_mat(size:int, limit:int):
+    "Generates invertible matrixes of n*n elements. limit sets an approximate size of element"
+    matrix = np.eye(size)
+    old_matrix = np.eye(size)
+
+    for i in range(size):
+        for j in range(size):
+            if i != j:
+                matrix[i] += old_matrix[j]*random.randint(-limit,limit)  
+        
+    for i in range(size):
+        matrix[i][i] *= random.randint(-limit, limit)
+    
+    return matrix 
+
+
+def determinant(matrix:np.ndarray):
+    size = matrix.shape[0]
+    
+    if size == 1:
+        return matrix[0][0]
+    else:
+        det = 0
+        for i in range(size):
+            indxs = [j for j in range(size) if j != i]
+            rows = np.array([[j]*(size-1) for j in range(1,size)])
+            cols = np.array([indxs]*(size-1))
+            det += (-1)**(i)*matrix[0][i]*determinant(matrix[rows, cols])
+        return det
+
+def matrixToSLE(matrix:np.ndarray, limit:int):
+    "Returns a column of X and f"
+    size = matrix.shape[0]
+    x = np.array([random.randint(-limit, limit) for i in range(size)])
+    f = matrix @ x
+
+    print(matrix, x, f)
+
+    return x, f
